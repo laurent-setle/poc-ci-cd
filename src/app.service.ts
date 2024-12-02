addc2b5566c7fb70b6e3eb2ff196904a6571407a
Laurent Verbruggen <laurent@setle.app>
2024-12-02 15:26:16 +0100

    Fix test

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   getHello(): string {
-    const environment = this.configService.get<string>('ENV', 'fallback');
+    const environment = this.configService.get<string>('ENV', 'development');
     return `Hello World in ${environment}! Test 1`;
   }
 }
